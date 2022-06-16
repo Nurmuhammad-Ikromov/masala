b972e9e319daba8d6a4c7d3ee353efc606597fa9
Nurmuhammad-Ikromov <Nurmuhammadikromov58@gmail.com>
2022-06-17 00:30:06 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+// ------------------------------- 1-masala ------------------------------------------------
+
+function addition(a, b) {
+	return a + b;
+}
+
+//---------------------------------- 2-masala ----------------------------------------------------
+
+function convert(minutes) {
+	return minutes*60;
+}
+
+//------------------------------------- 3-masala -----------------------------------------------
+
+function addition(num) {
+	return ++num;
+}
+
+// -------------------------------------4-masala-------------------------------------------------
+
+function addition(num) {
+	return ++num;
+}
+
+// -------------------------------------5-masala-------------------------------------------------
+
+function calcAge(age) {
+	return age*365;
+}
+
+// -------------------------------------6-masala-------------------------------------------------
+
+function remainder(x, y) {
+	return x%y;
+}
+
+// -------------------------------------7-masala-------------------------------------------------
+
+function footballPoints(wins, draws, losses) {
+	return 3*wins+1*draws+0*losses
+}
+// -------------------------------------8-masala-------------------------------------------------
+
+function points(twoPointers, threePointers) {
+	return 2*twoPointers+3*threePointers
+}
+// -------------------------------------9-masala-----------------------------------------------
+
+function lessThan100(a, b) {
+	if (a + b < 100)
+		return true;
+	else return false
+}
+
+// -------------------------------------10-masala-------------------------------------------------
+
+function animals(chickens, cows, pigs) {
+	return 2*chickens+4*cows+4*pigs
+}
